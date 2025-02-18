b9871e54066ac6e46054cc4eddd057557ffac601
AITBOUKHA Elias <elias.aitboukha@assurance-maladie.fr>
2025-02-18 15:05:48 +0100

    tail, pos & tests

--- src/main/java/com/gitlab/sokoban/domain/model/Tile.java
+++ src/main/java/com/gitlab/sokoban/domain/model/Tile.java
@@ -1,21 +1,5 @@
 package com.gitlab.sokoban.domain.model;
 
-public class Tile {
-    private final Position position;
-    private final State state;
-
-    public Tile(Position position, State state) {
-        this.position = position;
-        this.state = state;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-
-    public State getState() {
-        return state;
-    }
-
+public record Tile(Position position, State state) {
 }
 
