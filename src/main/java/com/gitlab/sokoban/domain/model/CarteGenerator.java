165539a7cbda00201b6952750951389b9b9891c3
AITBOUKHA Elias <elias.aitboukha@assurance-maladie.fr>
2025-02-18 14:30:57 +0100

    base model

---
+++ src/main/java/com/gitlab/sokoban/domain/model/CarteGenerator.java
@@ -0,0 +1,7 @@
+package com.gitlab.sokoban.domain.model;
+
+import java.util.List;
+
+public class Carte {
+}
+
--- src/main/java/com/gitlab/sokoban/domain/model/CaseState.java
+++ src/main/java/com/gitlab/sokoban/domain/model/CaseState.java
@@ -1,1 +1,8 @@
 package com.gitlab.sokoban.domain.model;
+
+public enum CaseState {
+    EMPTY,
+    PLAYER,
+    BOX
+}
+
