e2a0961ca65d8c7ec3d18d9561d386756a334dcb
AITBOUKHA Elias <elias.aitboukha@assurance-maladie.fr>
2025-02-18 14:43:48 +0100

    pos

--- src/main/java/com/gitlab/sokoban/domain/model/CarteGenerator.java
+++ src/main/java/com/gitlab/sokoban/domain/model/CarteGenerator.java
@@ -1,6 +1,5 @@
 package com.gitlab.sokoban.domain.model;
 
-import java.util.List;
 
 public class CarteGenerator {
 }
--- src/main/java/com/gitlab/sokoban/domain/model/Pos.java
+++ src/main/java/com/gitlab/sokoban/domain/model/Pos.java
@@ -1,4 +1,20 @@
 package com.gitlab.sokoban.domain.model;
 
 public class Pos {
+    private final int x;
+    private final int y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
+
